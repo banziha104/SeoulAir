c2422eb1f6bd55e43a1e2b49e7aa06e0b227b844
iyeongjun <they6687@naver.com>
2017-10-11 15:33:31 +0900

    Add Recycler

--- app/src/main/java/com/veryworks/iyeongjun/seoulair/NewsActivity.java
+++ app/src/main/java/com/veryworks/iyeongjun/seoulair/NewsActivity.java
@@ -1,26 +1,39 @@
 package com.veryworks.iyeongjun.seoulair;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+
+import com.veryworks.iyeongjun.seoulair.domain.Data;
+import com.veryworks.iyeongjun.seoulair.domain.NewsData;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class NewsActivity extends AppCompatActivity implements NaverNewsParser.SetView {
     NaverNewsParser naverNewsParser;
+    @BindView(R.id.recycler) RecyclerView recycler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+        ButterKnife.bind(this);
         naverNewsParser();
     }
 
-    private void naverNewsParser(){
+    private void naverNewsParser() {
         naverNewsParser = new NaverNewsParser();
         naverNewsParser.setContext(this);
         naverNewsParser.execute();
     }
+
     @Override
     public void setView() {
-        Log.d("result","NewsActivity");
+        recycler.setAdapter(new RecyclerAdapter(NewsData.getInstance().getData()));
+        recycler.setLayoutManager(new LinearLayoutManager(this));
     }
 
 }
---
+++ app/src/main/java/com/veryworks/iyeongjun/seoulair/RecyclerAdapter.java
@@ -0,0 +1,56 @@
+package com.veryworks.iyeongjun.seoulair;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.veryworks.iyeongjun.seoulair.domain.Data;
+import com.veryworks.iyeongjun.seoulair.domain.Items;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by iyeongjun on 2017. 10. 11..
+ */
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+    Items[] items;
+    int MaxPostion = 20;
+    public RecyclerAdapter(Data data) {
+        items = data.getItems();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_item,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.setTitle(items[position].getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return MaxPostion;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+        @BindView(R.id.itemTxtTitle) TextView itemTxtTitle;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+        }
+        public void setTitle(String str){
+            itemTxtTitle.setText(str);
+        }
+    }
+}
