99f312bba90398d9cf7c93073872cd6a44b64a80
iyeongjun <they6687@naver.com>
2017-10-15 19:08:24 +0900

    Add BubblePicker

--- app/src/main/java/com/veryworks/iyeongjun/seoulair/WordFragment.java
+++ app/src/main/java/com/veryworks/iyeongjun/seoulair/WordFragment.java
@@ -1,11 +1,25 @@
 package com.veryworks.iyeongjun.seoulair;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.igalata.bubblepicker.BubblePickerListener;
+import com.igalata.bubblepicker.adapter.BubblePickerAdapter;
+import com.igalata.bubblepicker.model.BubbleGradient;
+import com.igalata.bubblepicker.model.PickerItem;
+import com.igalata.bubblepicker.rendering.BubblePicker;
+
+import org.jetbrains.annotations.NotNull;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 
 /**
@@ -13,6 +27,10 @@ import android.view.ViewGroup;
  */
 public class WordFragment extends Fragment {
 
+
+    @BindView(R.id.bubble)
+    BubblePicker bubblePicker;
+    Unbinder unbinder;
 
     public WordFragment() {
         // Required empty public constructor
@@ -23,7 +41,45 @@ public class WordFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_word, container, false);
+        View view = inflater.inflate(R.layout.fragment_word, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        setBubble();
+        return view;
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+    private void setBubble(){
+        bubblePicker.setBubbleSize(5);
+        bubblePicker.setAdapter(new BubblePickerAdapter() {
+            @Override
+            public int getTotalCount() {
+                return 30;
+            }
+
+            @NotNull
+            @Override
+            public PickerItem getItem(int position) {
+                PickerItem item = new PickerItem();
+                item.setTitle(position+"");
+                item.setGradient(new BubbleGradient(Color.RED,Color.RED, BubbleGradient.VERTICAL));
+                item.setTextColor(ContextCompat.getColor(getActivity(), android.R.color.white));
+                return item;
+            }
+        });
+        bubblePicker.setListener(new BubblePickerListener() {
+            @Override
+            public void onBubbleSelected(@NotNull PickerItem pickerItem) {
+
+            }
+
+            @Override
+            public void onBubbleDeselected(@NotNull PickerItem pickerItem) {
+
+            }
+        });
+    }
 }
