b498575209da331fa92ae65e244ce778fb5f9681
iyeongjun <they6687@naver.com>
2017-10-15 19:26:21 +0900

    Add BubblePicker

--- app/src/main/java/com/veryworks/iyeongjun/seoulair/WordFragment.java
+++ app/src/main/java/com/veryworks/iyeongjun/seoulair/WordFragment.java
@@ -40,7 +40,6 @@ public class WordFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_word, container, false);
         unbinder = ButterKnife.bind(this, view);
         setBubble();
